2c0db22b77c035a06e636466609601059c556dd9
Damilare Joseph <dre4success@live.com>
2017-09-16 20:42:11 +0100

    toggling between signin and signout

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-
-	authButton() {
-		if(this.props.auth) {
-			return <Link to="/" className="nav-link">Sign Out</Link>
+	renderList() {
+		if (this.props.auth) {
+			return (
+				<li className="nav-item">
+					<Link to="/signout" className="nav-link">
+						Sign Out
+					</Link>
+				</li>
+			);
 		}
-		return <Link to="/signin" className="nav-link">Sign In</Link>
-		return <Link to="/signup" className="nav-link">Sign Up</Link>
+		return [
+			<li className="nav-item" key="1">
+				<Link to="/signin" className="nav-link">
+					Sign In
+				</Link>
+			</li>,
+			<li className="nav-item" key="2">
+				<Link to="/signup" className="nav-link">
+					Sign Up
+				</Link>
+			</li>
+		];
 	}
 
 	render() {
 		return (
 			<nav className="navbar navbar-light">
-				<Link to="/" className="navbar-brand">Redux Auth</Link>
-				<ul className="nav navbar-nav">
-					<li className="nav-item">{this.authButton()}</li>
-				</ul>
+				<Link to="/" className="navbar-brand">
+					Redux Auth
+				</Link>
+				<ul className="nav navbar-nav">{this.renderList()}</ul>
 			</nav>
 		);
 	}
 }
 
 function mapStateToProps(state) {
-	return { auth: state.auth.authenticated}
+	return { auth: state.auth.authenticated };
 }
 
 export default connect(mapStateToProps)(Header);

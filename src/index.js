02f36b0f0dbc0386365172189559ddd1251f2404
Damilare Joseph <dre4success@live.com>
2017-09-16 19:08:32 +0100

    dependecies

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import './index.css';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>

8c1be45435a83524286d7f3676d4ec2a0f26e2f6
Damilare Joseph <dre4success@live.com>
2017-09-16 06:13:24 +0100

    adding more components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter, Route} from 'react-router-dom';
+import Header from './header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }

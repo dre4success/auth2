d635a015030d75452f70f087332a5f7c3fcbadc5
Damilare Joseph <dre4success@live.com>
2017-09-17 12:09:12 +0100

    array to loop over names and tags

---
+++ src/components/auth/formField.js
@@ -0,0 +1,5 @@
+export default [
+	{name: 'email', value: 'Please enter an email'},
+	{name: 'password', value: 'Please enter a password'},
+	{name: 'pword', value: 'Please confirm your password'}
+]
